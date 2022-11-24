8f4fd8f9d908fde3e1d41032a875a0b4b38785a9
Anastasia <kazantseva_nastya_95@mail.ru>
2022-11-24 21:34:23 +0500

    создали метод на перебор массива и поиск нужной длины элемента, перезаписали в новый массив

--- Program.cs
+++ Program.cs
@@ -4,4 +4,17 @@ Console.Clear();
 string[] array = { "Hello", "2", "world", ":-)", "go", "pen" };
 string[] array1 = new string[array.Length];
 
-int maxWordLength = 3;н
+int maxWordLength = 3;
+
+void MassivPhrases(string[] arr, string[] arr1)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i].Length <= maxWordLength)
+    {
+      arr1[count] = arr[i];
+      count++;
+    }
+  }
+}
