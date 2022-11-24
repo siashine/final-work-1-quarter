2560a58380f6a0f282cbcf29d3116cd737ce52c4
Anastasia <kazantseva_nastya_95@mail.ru>
2022-11-24 21:38:56 +0500

    метод принт

--- Program.cs
+++ Program.cs
@@ -18,3 +18,12 @@ void MassivPhrases(string[] arr, string[] arr1)
     }
   }
 }
+
+void PrintArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write(($"{array[i]} "));
+  }
+  Console.WriteLine();
+}
